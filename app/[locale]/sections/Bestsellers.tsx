6d4b127164d291ebe2c42040488622d8ebe8e11b
blackweeed <blackweeeed@gmail.com>
2023-08-06 13:45:37 +0200

    fixed some bugs

--- app/[locale]/sections/Bestsellers.tsx
+++ app/[locale]/sections/Bestsellers.tsx
@@ -44,51 +44,44 @@ const Bestsellers = ({ products }) => {
         <nav className="flex gap-10 list-none mt-4"></nav>
       </div>
       <div className="w-full  grid grid-cols-1 lg:grid-cols-4 gap-y-10 gap-x-5">
-        {products
-          ?.sort(() => Math.random() - 0.5)
-          .slice(0, 8)
-          .map((product) => (
+        {products.slice(0, 8).map((product) => (
+          <div
+            className="w-full h-full relative cursor-pointer product"
+            key={product.id}
+          >
             <div
-              className="w-full h-full relative cursor-pointer product"
-              key={product.id}
+              onClick={() => handleAddToWishlist(product.attributes.slug)}
+              className="absolute flex items-center justify-center rounded bg-white w-12 h-10 top-4 right-0 lg:right-0 z-10"
             >
-              <div
-                onClick={() => handleAddToWishlist(product.attributes.slug)}
-                className="absolute flex items-center justify-center rounded bg-white w-12 h-10 top-4 right-0 lg:right-0 z-10"
-              >
-                {isProductInWishlist(product.attributes.slug) ? (
-                  <AiFillStar className="fill-red" size={28} />
-                ) : (
-                  <AiOutlineStar className="fill-black/80" size={28} />
-                )}
-              </div>
-
-              <Link
-                href={`/products/${product.attributes.slug}`}
-                className="product-img h-full"
-              >
-                {product.attributes?.coverImages?.data?.[0]?.attributes
-                  ?.url && (
-                  <img
-                    className="object-cover"
-                    src={product.attributes.coverImages.data[0].attributes.url}
-                    alt=""
-                  />
-                )}
-                {product.attributes?.coverImages?.data?.[1]?.attributes
-                  ?.url && (
-                  <img
-                    src={product.attributes.coverImages.data[1].attributes.url}
-                    alt=""
-                    className="rear-img object-cover"
-                  />
-                )}
-                <h2 className="font-medium text-lg">
-                  {product.attributes.name}
-                </h2>
-              </Link>
+              {isProductInWishlist(product.attributes.slug) ? (
+                <AiFillStar className="fill-red" size={28} />
+              ) : (
+                <AiOutlineStar className="fill-black/80" size={28} />
+              )}
             </div>
-          ))}
+
+            <Link
+              href={`/products/${product.attributes.slug}`}
+              className="product-img h-full"
+            >
+              {product.attributes?.coverImages?.data?.[0]?.attributes?.url && (
+                <img
+                  className="object-cover"
+                  src={product.attributes.coverImages.data[0].attributes.url}
+                  alt=""
+                />
+              )}
+              {product.attributes?.coverImages?.data?.[1]?.attributes?.url && (
+                <img
+                  src={product.attributes.coverImages.data[1].attributes.url}
+                  alt=""
+                  className="rear-img object-cover"
+                />
+              )}
+              <h2 className="font-medium text-lg">{product.attributes.name}</h2>
+            </Link>
+          </div>
+        ))}
       </div>
       <Link href="/search" className="button-primary mt-14 lg:mt-18 !w-fit">
         {t("check-more")}
