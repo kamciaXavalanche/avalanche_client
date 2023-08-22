2f908b060c8a59ce3f0c27e0b4712e365d6eb2fc
blackweeed <blackweeeed@gmail.com>
2023-08-22 10:39:33 +0200

    fixed url

--- app/[locale]/constants/constants.ts
+++ app/[locale]/constants/constants.ts
@@ -1,2 +1,2 @@
-export const url = "http://127.0.0.1:1337";
-// export const url = process.env.NEXT_PUBLIC_API_URL;
+// export const url = "http://127.0.0.1:1337";
+export const url = process.env.NEXT_PUBLIC_API_URL;
