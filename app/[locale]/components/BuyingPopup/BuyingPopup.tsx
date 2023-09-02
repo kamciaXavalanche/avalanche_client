966e369711f9388c65559c0b731aa6e4f65c10ac
blackweeed <blackweeeed@gmail.com>
2023-09-02 17:11:55 +0200

    feat: fixed buying popup translations

--- app/[locale]/components/BuyingPopup/BuyingPopup.tsx
+++ app/[locale]/components/BuyingPopup/BuyingPopup.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
-import React from "react";
+import { useTranslations } from "next-intl";
 
 const BuyingPopup = ({ setPopup, name }) => {
+  const t = useTranslations("buying-popup");
+
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center w-full h-screen z-50 bg-black/80">
       <div className="py-10  lg:w-[30%] lg:h-[40%] bg-white shadow-md text-black flex flex-col gap-2 px-12 items-center justify-center ">
         <h2>
-          Dodałeś do koszyka <span className="font-semibold">{name}</span>
+          {t("add-message")} <span className="font-semibold">{name}</span>
         </h2>
         <button onClick={() => setPopup(false)} className="button-primary mt-4">
-          Kontynuj zakupy
+          {t("button1")}
         </button>
         <Link href="/cart" className="button-secondary text-center">
-          Przejdź do zamówienia
+          {t("button2")}
         </Link>
       </div>
     </div>
