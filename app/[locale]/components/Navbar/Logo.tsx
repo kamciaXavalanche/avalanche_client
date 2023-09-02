461f2ee5b20c4480410582ac178d85aa78c9d14f
blackweeed <blackweeeed@gmail.com>
2023-09-02 17:09:16 +0200

    feat: added logo

--- app/[locale]/components/Navbar/Logo.tsx
+++ app/[locale]/components/Navbar/Logo.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
+import logo from "./logo.svg";
+import Image from "next/image";
 
 const Logo = () => {
-  return <Link href="/">LEVARDE</Link>;
+  return (
+    <Link href="/">
+      <Image src={logo} width={160} height={70} alt="logo" />
+    </Link>
+  );
 };
 
 export default Logo;
